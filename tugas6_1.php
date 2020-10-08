de9094d8f93fb3b957933510484c0f21555ac3c9
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-08 12:55:54 +0700

    Add files via upload

---
+++ tugas6_1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+    $habib= 21;
+    $aji = 4 + $habib;
+    $bintan = 3 + $aji;
+
+     ?>
+
+
+    =======================================
+    <br>
+    Usia Habib = <?php  echo $habib?>
+    <br>
+    Usia Aji = <?php  echo $aji?>
+    <br>
+    Usia Bintan = <?php  echo $bintan?>
+    <br>
+    =======================================
+
+
+  </body>
+</html>
